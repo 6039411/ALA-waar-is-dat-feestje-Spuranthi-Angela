86871310ac3d81257c6c381ebe1b16ec646107f3
6039411 <6039411@mborijnland.nl>
2026-03-11 13:12:59 +0100

    Login auth
    auth

--- Models/userConn.php
+++ Models/userConn.php
@@ -25,5 +25,41 @@ class User {
         }
     }
     
+    function auth($user_name, $user_password) {
+        try {
+            $sql = 'SELECT * FROM ' . $this->table_name . ' WHERE user_name = ?';
+            $stmt = $this->conn->prepare($sql);
+            $res = $stmt->execute([$user_name]);
+            if($stmt->rowCount() == 1){
+                $user = $stmt->fetch();
+                
+                $db_user_name = $user['user_name'];
+                $db_user_password = $user['user_password'];
+                $db_user_id = $user['user_id'];
+                $db_user_email = $user['user_email'];
+                $db_user_full_name = $user['user_full_name'];
+                if($db_user_name === $user_name){
+                    if (password_verify($user_password, $db_user_password)){
+                        $this->user_name = $db_user_name;
+                        $this->user_id = $db_user_id;
+                        $this->user_email = $db_user_email;
+                        $this->user_full_name = $db_user_full_name;
+                        return 1;
+                    }else return 0;   
+                }else return 0;   
+            }else return 0; 
+        } catch(PDOException $e) {
+            return 0; 
+        }
+    }
+
+    function getUser(){
+        $data = array('user_id' => $this->user_id,
+                      'user_name' => $this->user_name,
+                      'user_email' => $this->user_email,
+                      'user_full_name' => $this->user_full_name);
+        return $data;
+    }
+    
 }
 ?>
